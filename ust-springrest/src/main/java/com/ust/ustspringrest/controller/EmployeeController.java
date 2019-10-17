6a33fd8e53f681c9da7629ce186ac2e28d0ca509
palak9801 <53752703+palak9801@users.noreply.github.com>
2019-10-17 12:22:24 +0530

    commit spring rest

---
+++ ust-springrest/src/main/java/com/ust/ustspringrest/controller/EmployeeController.java
@@ -0,0 +1,130 @@
+package com.ust.ustspringrest.controller;
+
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ust.ustspringrest.dto.Employee;
+import com.ust.ustspringrest.dto.EmployeeResponse;
+import com.ust.ustspringrest.service.EmployeeService;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService service;
+	
+	@InitBinder
+	public void  initBinder(WebDataBinder binder) {
+		
+		SimpleDateFormat format = new  SimpleDateFormat("yyyy-mm-dd");
+		CustomDateEditor editor = new CustomDateEditor(format, true);
+		binder.registerCustomEditor(Date.class, editor);
+		
+		
+	}
+	
+	@GetMapping(path="/get", produces = MediaType.APPLICATION_JSON_VALUE)  //produces  to produces JSON data
+	public EmployeeResponse getEmployee(@RequestParam("id")int id) {           //@ResponseBody is use to tell that it is a actual content
+		                                                                               //it is difficult to specify it for each method therefore we will use @RestController to the class instead of Controller
+		Employee employee = service.getEmployee(id);
+		EmployeeResponse response = new EmployeeResponse();
+		if(employee == null) {
+			response.setStatusCode(401);
+			response.setMessage("Failure");
+			response.setDescription("Data not present ");
+		}else {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data found ");
+			response.setEmployees(Arrays.asList(employee));
+		}
+		return response;
+		}
+	                                                                  
+	@GetMapping(path="/get-all", produces = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse getAllEmployee(){
+		
+	List<Employee> employee =  service.getAllEmployees();
+		EmployeeResponse response = new EmployeeResponse();
+		if(employee == null) {
+			response.setStatusCode(401);
+			response.setMessage("Failure");
+			response.setDescription("Data not present ");
+		}else {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data found ");
+	        response.setEmployees(employee);
+	        
+		}
+		
+		return  response;
+	}
+	
+	@PostMapping(path="/add", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse addEmployee(@RequestBody Employee employee) {    //this annotation convert json or xml object into java object
+		EmployeeResponse response = new EmployeeResponse();
+		if(service.addEmployee(employee)) {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data Added Successfully");
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failure");
+			response.setDescription("Data not Added ");
+			}
+		return response;
+	}
+	
+	@PutMapping(path="/modify", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse modifyEmployee( @RequestBody Employee employee) {
+		EmployeeResponse response = new EmployeeResponse();
+		if(service.modifyEmployee(employee)) {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data Modified Successfully");
+			
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failure");
+			response.setDescription("Data not Modified ");
+		}
+		return response;
+	}
+	
+	@DeleteMapping(path="/remove/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public EmployeeResponse removeEmployee(@PathVariable("id")int id) {
+		EmployeeResponse response = new EmployeeResponse();
+		if(service.removeEmployee(id)) {
+			response.setStatusCode(201);
+			response.setMessage("Success");
+			response.setDescription("Data Removed Successfully");
+		}else {
+			response.setStatusCode(401);
+			response.setMessage("Failure");
+			response.setDescription("Data not Deleted ");
+		}
+		return response;
+	}
+}
---
+++ ust-springrest/src/main/java/com/ust/ustspringrest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.ust.ustspringrest.service;
+
+import java.util.List;
+
+import com.ust.ustspringrest.dto.Employee;
+
+public interface EmployeeService {
+	
+	public boolean addEmployee(Employee employee);
+	public boolean removeEmployee(int id);
+	public boolean modifyEmployee(Employee employee);
+	public Employee getEmployee(int id);
+	public List<Employee> getAllEmployees();
+	
+
+}
